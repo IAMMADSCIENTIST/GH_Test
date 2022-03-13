58b15fbb825fced1460b1bd4a2dd58376f01b069
IAMMADSCIENTIST <101133914+IAMMADSCIENTIST@users.noreply.github.com>
2022-03-13 17:01:31 +0100

    init
    initial commit

---
+++ Source/GH_Test/GH_TestGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GH_TestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GH_TEST_API AGH_TestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
